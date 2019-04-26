5b1091cd958ed53df840697b8d9e626e581b23ad
tymone <tymoteusz.hryniewiecki@gmail.com>
2019-04-26 17:30:12 +0200

    Updates

---
+++ precache-manifest.7621821dba4a5f1ce9eda95bc909f71c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "db3e3524abf64da230a4",
+    "url": "/ownsite/static/css/main.17972d5e.chunk.css"
+  },
+  {
+    "revision": "db3e3524abf64da230a4",
+    "url": "/ownsite/static/js/main.c0074607.chunk.js"
+  },
+  {
+    "revision": "36818fd89d30bc0041ab",
+    "url": "/ownsite/static/js/runtime~main.6ea42e7e.js"
+  },
+  {
+    "revision": "9ae159af9e64594aad19",
+    "url": "/ownsite/static/js/2.3197608f.chunk.js"
+  },
+  {
+    "revision": "cd03aadb8ad3daa9bb59ceff660da568",
+    "url": "/ownsite/static/media/html_logo.cd03aadb.png"
+  },
+  {
+    "revision": "ed7ebdd8ae4eca5b04948e17c92a6e3a",
+    "url": "/ownsite/static/media/css_logo.ed7ebdd8.png"
+  },
+  {
+    "revision": "88ed1acb1ccd7ee77868652da990d00f",
+    "url": "/ownsite/static/media/js_logo.88ed1acb.png"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/ownsite/static/media/react_logo.82a318cc.png"
+  },
+  {
+    "revision": "e6e29a2df57a504457dc09b2f1ae6b9c",
+    "url": "/ownsite/static/media/google_logo.e6e29a2d.png"
+  },
+  {
+    "revision": "bb6df76c0059b98e7719a30dadd60d48",
+    "url": "/ownsite/static/media/printscreen.bb6df76c.png"
+  },
+  {
+    "revision": "61e3a3939c2f93f30ac21419625c9a4f",
+    "url": "/ownsite/static/media/header.61e3a393.jpg"
+  },
+  {
+    "revision": "7ae7622a8c4cd349370fee47c214b86b",
+    "url": "/ownsite/static/media/mountain.7ae7622a.jpg"
+  },
+  {
+    "revision": "b7548d37fa9d38ad398cb3879dda70e7",
+    "url": "/ownsite/static/media/bjj.b7548d37.jpg"
+  },
+  {
+    "revision": "8820c88fe0617aab45701804b4325313",
+    "url": "/ownsite/static/media/swimming.8820c88f.jpg"
+  },
+  {
+    "revision": "d039d58ade19b6bba1cf699b81fad215",
+    "url": "/ownsite/static/media/running.d039d58a.jpg"
+  },
+  {
+    "revision": "cdf7fb20b4be1936d065d7763b1306ad",
+    "url": "/ownsite/index.html"
+  }
+];
