0f52db32c4922379cfee7912f829244068a2b3a6
tymone <tymoteusz.hryniewiecki@gmail.com>
2019-04-27 16:31:29 +0200

    add few correct to finish version 1.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,70 +1,68 @@
 //poprawić IF'y w hideNav!
 
-import React from 'react';
-import '../styles/Nav.css';
-import $ from 'jquery';
-
+import React from "react";
+import "../styles/Nav.css";
+import $ from "jquery";
 
 export default class Nav extends React.Component {
-
   navbarFunc = () => {
-    $('.fa-times, nav, ul, li').toggleClass('active');
-  }
+    $(".fa-times, nav, ul, li").toggleClass("active");
+  };
 
-  hideNav = () => {
-    $(document).on('scroll', function () {
+  backgroundScroll = () => {
+    $(document).on("scroll", function() {
       const scroll = $(document).scrollTop();
-      const headerHigh = $('Header').height();
-      const windowWidth = $(document).outerWidth();
-      const nav = $('nav');
-      const ul = $('ul');
-      const li = $('li');
-      const i = $('li>i');
-      if (windowWidth < 601) {
-        nav.removeClass('sideNav');
-        ul.removeClass('sideNav');
-        li.removeClass('sideNav');
-        i.removeClass('sideNav');
-      } else if (scroll > headerHigh) {
-        nav.addClass('sideNav');
-        ul.addClass('sideNav');
-        li.addClass('sideNav');
-        i.addClass('sideNav');
-      } else {
-        nav.removeClass('sideNav');
-        ul.removeClass('sideNav');
-        li.removeClass('sideNav');
-        i.removeClass('sideNav');
+      const headerHigh = $("header").height();
+      const $nav = $("nav");
+      if (scroll > headerHigh - 300) {
+        $nav.addClass("blackBgc");
       }
-    })
-  }
+      if (scroll < headerHigh - 300) {
+        $nav.removeClass("blackBgc");
+      }
+    });
+  };
 
   scrollOnSection1 = () => {
-    $('body, html').animate({ scrollTop: $('.wrap').offset().top })
-  }
+    $("body, html").animate({ scrollTop: $(".wrap").offset().top });
+    $(".fa-times, nav, ul, li").removeClass("active");
+  };
   scrollOnSection2 = () => {
-    $('body, html').animate({ scrollTop: $('.project').offset().top })
-  }
+    $("body, html").animate({ scrollTop: $(".project").offset().top });
+    $(".fa-times, nav, ul, li").removeClass("active");
+  };
   scrollOnSection3 = () => {
-    $('body, html').animate({ scrollTop: $('.about').offset().top })
-  }
+    $("body, html").animate({ scrollTop: $(".about").offset().top });
+    $(".fa-times, nav, ul, li").removeClass("active");
+  };
   scrollOnSection4 = () => {
-    $('body, html').animate({ scrollTop: $('.contact').offset().top })
-  }
-
+    $("body, html").animate({ scrollTop: $(".contact").offset().top });
+    $(".fa-times, nav, ul, li").removeClass("active");
+  };
 
   render() {
-    this.hideNav();
+    this.backgroundScroll();
     return (
       <nav>
-        <i className="fas fa-times" onClick={this.navbarFunc}></i>
+        <i className="fas fa-times" onClick={this.navbarFunc} />
         <ul>
-          <li onClick={this.scrollOnSection1}><i className="fas fa-home"></i>Start</li>
-          <li onClick={this.scrollOnSection2}><i className="fas fa-project-diagram"></i>Projekty</li>
-          <li onClick={this.scrollOnSection3}><i className="fas fa-user-tie"></i> O mnie</li>
-          <li onClick={this.scrollOnSection4}><i className="fas fa-address-card"></i>Kontakt</li>
+          <li onClick={this.scrollOnSection1}>
+            <i className="fas fa-home" />
+            Start
+          </li>
+          <li onClick={this.scrollOnSection2}>
+            <i className="fas fa-project-diagram" />
+            Projekty
+          </li>
+          <li onClick={this.scrollOnSection3}>
+            <i className="fas fa-user-tie" /> O mnie
+          </li>
+          <li onClick={this.scrollOnSection4}>
+            <i className="fas fa-address-card" />
+            Kontakt
+          </li>
         </ul>
       </nav>
-    )
+    );
   }
 }
